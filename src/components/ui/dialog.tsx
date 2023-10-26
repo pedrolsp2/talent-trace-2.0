0d986da039015fb44f4311d36545485c94a08ab9
Pedro <pedro.lucassofia@gmail.com>
2023-10-26 18:35:18 -0300

    Dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -33,7 +33,7 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
-  <DialogPortal className="">
+  <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
